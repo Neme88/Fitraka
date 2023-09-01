ecbdd2261c2040417baeda575da24b1bc81dff78
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-09-01 17:25:28 +0200

    updated run and requirments.txt file files

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 # run.py
 
 from Fitraka import create_app
-from config import Config  # Import your configuration class
 
 # Use the create_app function to create the Flask app with your configuration class
-Fitraka = create_app()
+app = create_app()
+#if __name__ == "__main__":
+#    app.run(debug=False)
 
-if __name__ == '__main__':
-    Fitraka.run(debug=True)
 
